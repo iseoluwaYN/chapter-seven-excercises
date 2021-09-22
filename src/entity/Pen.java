50bccf38d6bc061c36f33c851c85d6aef8e6ade6
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2021-09-22 18:02:37 +0100

    Pen

---
+++ src/entity/Pen.java
@@ -0,0 +1,16 @@
+package entity;
+
+import enums.PenPosition;
+
+public class Pen {
+    private PenPosition penPosition;
+
+
+    public PenPosition getPenPosition() {
+        return penPosition;
+    }
+
+    public void setPenPosition(PenPosition penPosition) {
+        this.penPosition = penPosition;
+    }
+}
